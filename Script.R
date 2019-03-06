7ee2c9a46d9acc3908ec068d6d3035c137f140f9
verohan <39477779+verohan@users.noreply.github.com>
2019-03-06 13:44:43 +0800

    Create Script.R

---
+++ Script.R
@@ -0,0 +1,8 @@
+color_list <- list()
+color_list$E14 <- c('cycle-related' = '#2ca02c','restricted-RPC'='#d62728','c4'='#9467bd','RGC2'='#8c564b','RGC1'='#e377c2','pre-RGC'='#bcbd22','pre-PRC'='#17becf','pre-AC/HC'= '#aec7e8')
+color_list$E17 <- c('cycle-related'= 'forestgreen','restricted-RPC'='#d62728','RGC'='#8c564b','pre-PRC'='#17becf','pre-AC/HC'= '#aec7e8','AC/HC' = '#bcbd22','c07' = '#E3E36A','PRC' = '#D09E88','c11'= '#f100e5','c12' = '#519D9E' )
+color_list$P3 <- c('p00'= '#f857b5', 'pre-PRC' ='#17becf', 'p02' = '#a7d129','AC/HC-1' = '#df4d19','Nrl+PRC' = '#ff467e','pre-AC/HC' = '#aec7e8','PRC' = '#D09E88','AC/HC-2' = '#9a1ba0','cone-PRC' = '#f6ec66','Gad2+AC' = '#79b8d1','Neurog2+'= '#0d7e83','RGC' = '#8c564b','p15' = '#658525','Calb1/Chat-AC' = '#fda403','early-muller' ='#3a0088')
+
+
+
+
