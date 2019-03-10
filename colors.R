ca411830d41ec165d690e420d515fdc0d802209b
verohan <39477779+verohan@users.noreply.github.com>
2019-03-10 14:36:26 +0800

    Create colors.R

---
+++ colors.R
@@ -0,0 +1,3 @@
+Cluster_raw <- c('AC/HC' = '#f36a7b','PR' = '#a6cb12', 'RPC' = '#f857b5', 
+                 'RGC' = '#8f71ff', 'Precursors' = '#13abc4', 'Restricted-RPC' = '#26baee',
+                 'C0' = '#f8b500')
